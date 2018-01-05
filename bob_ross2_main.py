f1a2e9e74c5f09b5f4259808b39a1854b2ea7ec8
Frank Wills <frank.s.wills@gmail.com>
2018-01-04 22:12:04 -0500

    created basic frame of br2 based off readme outline

--- bob_ross2_main.py
+++ bob_ross2_main.py
@@ -1,2 +1,37 @@
 import pygame
+#TODO: decide if numpy actually improves performance on translations or not
 import numpy
+
+class bob_ross2():
+
+    # the color currently selected
+    current_color = [255,255,255]
+
+    # a matrix of dots x and y positions
+    dot_matrix = []
+
+
+    ## Translational Effects
+    def circle_trans():
+        pass
+
+    def wave_trans():
+        pass
+
+    def random_trans():
+        pass
+
+
+    ## Color Effects
+    def rainbow():
+        pass
+
+    def sync_color():
+
+
+
+    def draw_dots(dot_list):
+        pass
+
+    def __main__():
+        pass
