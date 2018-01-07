db1eebe0bcd55d6b97994ec6173d393bff8059bf
Frank Wills <frank.s.wills@gmail.com>
2018-01-07 16:16:20 -0500

    created circle trans and began to write main

--- bob_ross2_main.py
+++ bob_ross2_main.py
@@ -4,16 +4,30 @@ import numpy
 
 class bob_ross2():
 
+    screen_size = (1920, 1080) #TODO: make this draw from windows metrics
+
     # the color currently selected
     current_color = [255,255,255]
 
-    # a matrix of dot objects
-    dot_matrix = []
+    # list of dot objects to be drawn
+    dot_list = []
+
+    # variables used for circle translations
+    center = (int(screen_size[0]/2), int(screen_size[1]/2))
+    angle_mod = 1 # set to a fraction of pi for symmetry
 
 
     ## Translational Effects
     def circle_trans():
-        pass
+        for dot in dot_list:
+            dot_pos = dot.get_pos()
+            new_x = ((dot_pos[0] - center[0]) * cos(angle_mod))
+            -((dot_pos[1] - center[1]) * sin(angle_mod)) + center[0]
+
+            new_y = ((dot_pos[1] - center[1]) * cos(angle_mod))
+            + ((dot_pos[0] - center[0]) * sin(angle_mod)) + center[1]
+
+            dot.set_pos(new_x, new_y)
 
     def wave_trans():
         pass
@@ -34,4 +48,12 @@ class bob_ross2():
         pass
 
     def __main__():
-        
+        exit = False # determines when to exit the loop
+
+        # pygame initialization
+        screen = pygame.display.set_mode(screen_size, pygame.FULLSCREEN)
+        pygame.display.set_caption("BOB ROSS")
+        pygame.init()
+
+        while exit is not True:
+            # checking inputs and applying effects
